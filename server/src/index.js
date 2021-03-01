4b98b7ce3a2c1fa022d0d67e71bc2389f1ff3bf2
leandrofahur <fahurleandro@gmail.com>
2021-02-28 18:25:14 -0800

    passport + passport-google-oauth20 and secret

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
+passport.use(new GoogleStrategy());
+
 const PORT = process.env.PORT | 5000;
-
-app.get("/", (req, res) => {
-  const obj = { hi: "there" };
-  res.send(obj);
-});
-
 app.listen(PORT, (req, res) => {
   console.log(`Server up and running on port ${PORT}`);
 });
